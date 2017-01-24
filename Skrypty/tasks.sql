2e704dfac2d2d24818b0728dd65aff474a3590a5
Rafał Piotrowski <s421809@shell.wmi.amu.edu.pl>
2017-01-24 21:14:38 +0100

    first procedure <3

--- Skrypty/tasks.sql
+++ Skrypty/tasks.sql
@@ -1,7 +1,29 @@
-Rejestracja nowego pacjenta, zmiana jego danych
+--Rejestracja nowego pacjenta, zmiana jego danych
 Dodanie nowego pracownika, zmiana jego danych
 Dopisanie pracownikowi nowej umiejêtnoœci (wykonania ustalonej us³ugi)
 Dodawanie nowych us³ug
 Umówienie wizyty pacjenta 
 Anulowanie wizyty
 Komentowanie wizyty przez pracownika z opisem przebiegu us³ugi
+
+
+create procedure addPatient
+        @pesel bigint,
+		@name varchar(20),
+		@surname varchar(20),
+		@address varchar(100),
+		@phone bigint,
+		@mail varchar(50),
+		@vip tinyint = 0
+as
+begin try
+        insert into Pacjenci
+		values (@pesel, @name,@surname,@address,@phone,@mail,@vip); 
+end try
+begin catch
+     print ERROR_MESSAGE() 
+end catch
+
+exec addPatient 95071912345,Monika,Tyblewska,"Niebieska 19 64-320 Otusz",678678678,"monia@wp.pl",1
+
+
